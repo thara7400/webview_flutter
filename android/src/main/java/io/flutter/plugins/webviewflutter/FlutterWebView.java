2fa720e0fcbba1d5b8033d9ef8007481ecd7d836
thara7400 <57885640+thara7400@users.noreply.github.com>
2020-03-07 11:45:37 +0900

    ViewPort handling.

--- android/src/main/java/io/flutter/plugins/webviewflutter/FlutterWebView.java
+++ android/src/main/java/io/flutter/plugins/webviewflutter/FlutterWebView.java
@@ -50,6 +50,10 @@ public class FlutterWebView implements PlatformView, MethodCallHandler {
     webView.getSettings().setDomStorageEnabled(true);
     webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
 
+    // ViewPort handling.
+    webView.getSettings().setUseWideViewPort(true);
+    webView.getSettings().setLoadWithOverviewMode(true);
+
     methodChannel = new MethodChannel(messenger, "plugins.flutter.io/webview_" + id);
     methodChannel.setMethodCallHandler(this);
 
